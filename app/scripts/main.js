2add2900d12372f88f4fb3220bfbd2f6f512eb12
Philipp Timmalog <ptimmalog@gmail.com>
2017-06-15 13:41:55 +0200

    adjust scrolling behaviour

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,4 +47,45 @@ $(document).ready(function () {
     });
   });
 
+  $(document).on("scroll", onScroll);
+
+  $('.lab-navigation a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+
+      $('.lab-navigation a').each(function () {
+          $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+
+      var target = this.hash;
+      var menu = target;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top+2
+      }, 500, 'swing', function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+      });
+  });
+
+  function onScroll(event) {
+      var scrollPos = $(document).scrollTop() + 100;
+      $('.lab-navigation a').each(function () {
+          var currLink = $(this);
+          var refElement = $(currLink).attr('href');
+          var $target = $(refElement);
+          if ($target.position().top <= scrollPos && $target.position().top + 100 + $target.height() > scrollPos) {
+              $('.lab-navigation a').removeClass("active");
+              currLink.addClass("active");
+          }
+          else {
+              currLink.removeClass("active");
+          }
+      });
+  }
+
+
+
 });
