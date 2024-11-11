52359260da3e01a26378b638681622ec40d90940
Philipp Timmalog <ptimmalog@gmail.com>
2024-11-11 16:07:25 +0100

    permissions

--- scripts/cleanup_nginx.sh
+++ scripts/cleanup_nginx.sh
@@ -3,8 +3,8 @@
 branch_name=$1
 
 # Remove NGINX config
-rm /etc/nginx/sites-available/$branch_name
-rm /etc/nginx/sites-enabled/$branch_name
+sudo rm /etc/nginx/sites-available/$branch_name
+sudo rm /etc/nginx/sites-enabled/$branch_name
 
 # Reload NGINX to apply changes
 sudo systemctl reload nginx
--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -4,7 +4,7 @@ branch_name=$1
 port=$2
 
 # Create NGINX config for the subdomain
-cat <<EOT > /etc/nginx/sites-available/$branch_name
+sudo cat <<EOT > /etc/nginx/sites-available/$branch_name
 server {
     server_name ${branch_name}--preview.t7lab.com;
     location / {
@@ -18,7 +18,7 @@ server {
 EOT
 
 # Symlink to sites-enabled
-ln -s /etc/nginx/sites-available/$branch_name /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/$branch_name /etc/nginx/sites-enabled/
 
 # Reload NGINX to apply changes
 sudo systemctl reload nginx
