655908827a5a75590ebaf035ea7e320a45d1978d
Philipp Timmalog <ptimmalog@gmail.com>
2024-11-13 09:03:52 +0100

    add github action scripts

---
+++ scripts/configure_caddy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+branch_name=$1
+port=$2
+delete_flag=$3
+
+caddyfile="/etc/caddy/Caddyfile"
+subdomain="${branch_name}--preview.t7lab.com"
+reverse_proxy_entry="http://${subdomain} {\n  reverse_proxy [::1]:${port}\n}\n"
+
+if [[ "$delete_flag" == "--delete" ]]; then
+    # Delete the existing entry for this subdomain
+    if grep -q "${subdomain}" "$caddyfile"; then
+        sudo sed -i "/http:\/\/${subdomain} {/,/}/d" "$caddyfile"
+        echo "Deleted entry for ${subdomain}."
+    else
+        echo "No entry found for ${subdomain}."
+    fi
+else
+  # Check if the branch is already present in the Caddyfile
+  if grep -q "${subdomain}" "$caddyfile"; then
+      # Use sed to replace the existing entry for this subdomain
+      sudo sed -i "/http:\/\/${subdomain} {/,/}/c\\${reverse_proxy_entry}" "$caddyfile"
+  else
+      # Append a new entry at the end of the Caddyfile
+      echo -e "$reverse_proxy_entry" | sudo tee -a "$caddyfile" > /dev/null
+  fi
+fi
+
+# Reload Caddy to apply changes
+sudo systemctl reload caddy
