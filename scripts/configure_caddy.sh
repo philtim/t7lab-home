724f676cc1cb77a5178a226fe54e415334a76c10
Philipp Timmalog <ptimmalog@gmail.com>
2024-11-13 15:15:50 +0100

    use ssl on caddy

--- scripts/configure_caddy.sh
+++ scripts/configure_caddy.sh
@@ -6,12 +6,12 @@ delete_flag=$3
 
 caddyfile="/etc/caddy/Caddyfile"
 subdomain="${branch_name}--preview.t7lab.com"
-reverse_proxy_entry="http://${subdomain} {\n  reverse_proxy [::1]:${port}\n}\n"
+reverse_proxy_entry="${subdomain} {\n  reverse_proxy [::1]:${port}\n}\n"
 
 if [[ "$delete_flag" == "--delete" ]]; then
     # Delete the existing entry for this subdomain
     if grep -q "${subdomain}" "$caddyfile"; then
-        sudo sed -i "/http:\/\/${subdomain} {/,/}/d" "$caddyfile"
+        sudo sed -i "/${subdomain} {/,/}/d" "$caddyfile"
         echo "Deleted entry for ${subdomain}."
     else
         echo "No entry found for ${subdomain}."
@@ -20,7 +20,7 @@ else
   # Check if the branch is already present in the Caddyfile
   if grep -q "${subdomain}" "$caddyfile"; then
       # Use sed to replace the existing entry for this subdomain
-      sudo sed -i "/http:\/\/${subdomain} {/,/}/c\\${reverse_proxy_entry}" "$caddyfile"
+      sudo sed -i "/${subdomain} {/,/}/c\\${reverse_proxy_entry}" "$caddyfile"
   else
       # Append a new entry at the end of the Caddyfile
       echo -e "$reverse_proxy_entry" | sudo tee -a "$caddyfile" > /dev/null
